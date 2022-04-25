feee82d8a9a0c7e44dc506a97d634ea1e842d8d5
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-04-25 16:17:53 +0630

    solved exercise 2_variables

---
+++ 2_variables.py
@@ -0,0 +1,20 @@
+"""
+Exercise: Python Variables
+Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+
+Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+Store your first, middle and last name in three different variables and then print your full name using these variables
+
+Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+"""
+
+birth_year = 1998
+current_year = 2022
+my_age = current_year - birth_year
+print('my age ', my_age)
+
+first_name = 'Khin'
+middle_name = 'Thandar'
+last_name = 'Kyaw'
+print('my name ', first_name, middle_name, last_name)
