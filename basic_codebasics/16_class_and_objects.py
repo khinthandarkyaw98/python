369f3a4117dc9cfbec06825d6330a149e4150323
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-02 21:01:35 +0630

    solved 16_class_and_objects.py

---
+++ basic_codebasics/16_class_and_objects.py
@@ -0,0 +1,28 @@
+class Employee:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def display(self):
+        print(f"id: {self.id}, name: {self.name}\n")
+
+emp = Employee(1, "coder")
+emp.display()
+
+del emp.id
+
+try:
+    print(emp.id)
+
+except Exception as e:
+    print('id is not found')
+
+del emp
+
+try:
+    print(emp)
+
+except Exception as e:
+    print('the object emp is not found')
+
+
