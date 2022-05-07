e421856fb0b918b14b71356ad4e77a89ded3f134
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-07 19:46:12 +0630

    added stacking.py

---
+++ numpy/stacking.py
@@ -0,0 +1,16 @@
+a = np.arange(6).reshape(3,2)
+b = np.arange(6,12).reshape(3,2)
+print()
+print("a = \n", a)
+print()
+print("b = \n", b)
+# output
+# a =
+#  [[0 1]
+#  [2 3]
+#  [4 5]]
+
+# b =
+#  [[ 6  7]
+#  [ 8  9]
+#  [10 11]]
