5d3e2490bc64d6b646b64d3078c27ed62a8c3d4d
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-07 14:58:06 +0630

    added ravel()

---
+++ numpy/flatten.py
@@ -0,0 +1,9 @@
+# make one dimensional array
+
+import numpy as np
+
+a = np.array([[1, 2], [3, 5], [2, 9]])
+
+b = np.ravel(a)
+
+print("After making 2-dimensional array into 1-dimensional array", b)
