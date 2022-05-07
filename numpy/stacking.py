e7acb66c146b096b82bb3d2056aee7f393cf3923
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-07 20:12:28 +0630

    updated stacking.py

--- numpy/stacking.py
+++ numpy/stacking.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 a = np.arange(6).reshape(3,2)
 b = np.arange(6,12).reshape(3,2)
 print()
@@ -14,3 +16,26 @@ print("b = \n", b)
 #  [[ 6  7]
 #  [ 8  9]
 #  [10 11]]
+
+c = np.vstack((a,b)) # tuple (())
+print(f"vertical_stacking = \n"
+      f"{c}")
+
+# output
+# vertical_stacking =
+# [[ 0  1]
+#  [ 2  3]
+#  [ 4  5]
+#  [ 6  7]
+#  [ 8  9]
+#  [10 11]]
+
+d = np.hstack((a,b)) # tuple (())
+print(f"horizontal_stacking = \n"
+      f"{d}")
+
+# output
+# horizontal_stacking =
+# [[ 0  1  6  7]
+#  [ 2  3  8  9]
+#  [ 4  5 10 11]]
