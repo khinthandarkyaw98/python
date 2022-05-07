3b22dd70a7d06a57003bc62fcc66cc5ae35e904f
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-07 14:40:07 +0630

    added np.zeros() and np.ones()

---
+++ numpy/row_col_initialization.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a = np.zeros( (3, 2) )
+
+b = np. ones( (2,2) )
+
+print(a)
+print()
+print(b)
+
+# the outputs are
+"""
+[[0. 0.]
+ [0. 0.]
+ [0. 0.]]
+
+[[1. 1.]
+ [1. 1.]]
+"""
