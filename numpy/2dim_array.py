7782c3dab99aad66d1953547e095df75ecfffb34
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-07 13:43:50 +0630

    added 2_dim_array

---
+++ numpy/2dim_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# create a 2d-array using numpy
+dim2 = np.array([[1,2], [3,2], [3,0]])
+
+# check the dimension of the numpy array
+print(f"The dimension of the numpy array is"
+      f"{dim2.ndim}")
