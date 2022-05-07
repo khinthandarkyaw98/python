e4c5ff73de7ede8e26e2a4a5930961f68f9f5ad5
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-07 20:58:13 +0630

    updated indexing_with_boolean_arrays.py

--- numpy/indexing_with_boolean_arrays.py
+++ numpy/indexing_with_boolean_arrays.py
@@ -3,3 +3,32 @@ import numpy as np
 a = np.arange(12).reshape(3, 4)
 
 print(f"a \n = {a}")
+
+# output
+# a
+#  = [[ 0  1  2  3]
+#  [ 4  5  6  7]
+#  [ 8  9 10 11]]
+
+b = a > 4
+
+print(f"b \n = {b}\n")
+# output
+# b
+#  = [[False False False False]
+#  [False  True  True  True]
+#  [ True  True  True  True]]
+
+c = a[b]
+print(f"a[b] = {c}")
+# output
+# a[b] = [ 5  6  7  8  9 10 11]
+
+# set all a > 4 to -1
+a[b] = -1 # c = -1 does not work
+print(f"\n a \n= {a}")
+# output
+#  a
+# = [[ 0  1  2  3]
+#  [ 4 -1 -1 -1]
+#  [-1 -1 -1 -1]]
