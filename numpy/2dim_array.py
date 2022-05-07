cf24d631b062556410173b5d6430cac110e2b613
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-07 15:04:03 +0630

    changed the format of the array

--- numpy/2dim_array.py
+++ numpy/2dim_array.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 # create a 2d-array using numpy
-dim2 = np.array([[1,2], [3,2], [3,0]])
+dim2 = np.array([[1,2],
+                 [3,2],
+                 [3,0]])
 
 # check the dimension of the numpy array
 print(f"The dimension of the numpy array is"
