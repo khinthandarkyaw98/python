37b79a63c1f994db22c0bcb8136498c3559dade5
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-05-11 14:50:59 +0630

    create a folder

--- numpy/flatten.py
+++
@@ -1,9 +0,0 @@
-# make one dimensional array
-
-import numpy as np
-
-a = np.array([[1, 2], [3, 5], [2, 9]])
-
-b = np.ravel(a)
-
-print("After making 2-dimensional array into 1-dimensional array", b)
