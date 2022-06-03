4f3d8da44ae15d44176b198b37694eea6e496bcf
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-06-03 22:15:08 +0630

    added my solution

---
+++ data_structure/4_nyc_weather_hash_table.py
@@ -0,0 +1,30 @@
+# my solution
+
+weather = []
+with open("nyc_weather.csv", "r") as f:
+    for line in f:
+        token = line.split(',')
+        date = token[0]
+        temperature = token[1]
+        if '\n' in temperature:
+            temperature = temperature[:len(temperature)-1]
+        weather.append([date, temperature])
+
+sum = 0
+arr = []
+for index, element in enumerate(weather):
+    if index > 0:
+        sum += int(element[1])
+
+        arr.append(int(element[1]))
+
+average = sum / index
+print("average temperature = ", average)
+print("maximum temperature = ", max(arr))
+
+#############################################################################
+
+
+
+
+
