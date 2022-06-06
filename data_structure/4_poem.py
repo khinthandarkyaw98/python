00b4040dadec2c505fd40ee98e877c7cc0b978ca
khinthandarkyaw <khinthanderkyaw1998@gmail.com>
2022-06-06 18:15:33 +0630

    solved problem 3

---
+++ data_structure/4_poem.py
@@ -0,0 +1,19 @@
+word_count = {}
+with open("poem.txt", "r") as f:
+    for line in f:
+        tokens = line.split(" ")
+        for token in tokens:
+            token = token.replace("\n", "")
+            token = token.replace(",", "")
+            token = token.replace(".", "")
+            token = token.replace(";", "")
+            token = token.replace('”', "")
+            token = token.replace(":", "")
+            if token in word_count:
+                word_count[token] += 1
+            else:
+                word_count[token] = 1
+
+print(word_count)
+
+
